b60ce82f6378971ca596972d8c7808de75132f29
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-10-25 17:06:17 +0100

    Refactor About page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,5 +1,5 @@
-import PlaceholderImage from '../images/placeholder.png'
 import styled from 'styled-components'
+import aboutInformation from './about-information'
 
 const AboutHeading = styled.h3`
   text-align: center;
@@ -16,35 +16,29 @@ const imageStyle = {
   width: '60%',
 }
 
+const Card = (props) => {
+  return (
+    <div className='about-column'>
+      <img src={props.image} alt='Wellbie' style={imageStyle}></img>
+      <AboutHeading>{props.heading}</AboutHeading>
+      <Paragraph>{props.content}</Paragraph>
+    </div>
+  )
+}
+
+const createCard = (cardName) => {
+  return (
+    <Card
+      image={cardName.image}
+      heading={cardName.heading}
+      content={cardName.content}
+    />
+  )
+}
 const AboutPage = () => (
   <div>
     <h2>About Wellbie</h2>
-    <div className='row'>
-      <div className='about-column'>
-        <img src={PlaceholderImage} alt='Wellbie' style={imageStyle}></img>
-        <AboutHeading>Holistic, personalised care in one app</AboutHeading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </Paragraph>
-      </div>
-      <div className='about-column'>
-        <img src={PlaceholderImage} alt='Wellbie' style={imageStyle}></img>
-        <AboutHeading>Clinically proven and safe</AboutHeading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </Paragraph>
-      </div>
-      <div className='about-column'>
-        <img src={PlaceholderImage} alt='Wellbie' style={imageStyle}></img>
-        <AboutHeading>Sustainable & affordable</AboutHeading>
-        <Paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </Paragraph>
-      </div>
-    </div>
+    <div className='row'>{aboutInformation.map(createCard)}</div>
   </div>
 )
 
---
+++ frontend/src/pages/about-information.js
@@ -0,0 +1,24 @@
+import PlaceholderImage from '../images/placeholder.png'
+
+const aboutInformation = [
+  {
+    image: PlaceholderImage,
+    heading: 'Holistic, personalised care in one app',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    image: PlaceholderImage,
+    heading: 'Clinically proven and safe',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    image: PlaceholderImage,
+    heading: 'Sustainable & affordable',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+]
+
+export default aboutInformation
