22284a692cc3eefe100dc4eae25bdfaace6d26a1
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-10-29 20:34:41 +0100

    Add cors headers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'tracker',
     'tracker_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,3 +132,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:3000",
+]
