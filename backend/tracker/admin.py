5cf89b77df520841f6a1f8a615128d8042fd2616
Onyela Ogah <83657534+onyela-o@users.noreply.github.com>
2022-11-11 00:23:16 +0000

    Remove backend

--- backend/tracker/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-
-@admin.register(models.Post)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ( 'id', 'category', 'experienced_today', 'severity', 'sleep','time', 'author' )
--- backend/tracker_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from tracker.models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ( 'id', 'category', 'experienced_today',  'severity', 'sleep', 'time', 'author' )
-
